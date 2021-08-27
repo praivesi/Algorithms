b5af27c0a34a78ac468997c2005b3f9ef9243a5f
praivesi <praivesi@gmail.com>
2021-08-28 00:24:29 +0900

    update AscendingNumber

--- src/DP/AscendingNumber.java
+++ src/DP/AscendingNumber.java
@@ -10,41 +10,38 @@ public class AscendingNumber {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        ascendingCounts = new int[n + 1][10];
-        for (int i = 0; i < n + 1; i++) {
+        ascendingCounts = new int[n][10];
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < 10; j++) {
                 ascendingCounts[i][j] = UNDEFINED_FLAG;
             }
         }
 
-        int result = 0;
-        for (int i = 0; i < 10; i++) {
-            result += getAscendingNumCnt(n, i);
-        }
-
-        result %= 10007;
+        int result = getAscendingNumCnt(n, 9);
 
         System.out.println(result);
         sc.close();
     }
 
     private static int getAscendingNumCnt(int n, int minNumber) {
-        if (ascendingCounts[n][minNumber] != UNDEFINED_FLAG) {
-            return ascendingCounts[n][minNumber];
-        }
-
         if (n == 1) {
-            return 1;
+            return minNumber + 1;
         }
 
-        ascendingCounts[n][minNumber] = 0;
+        int index = n - 1;
+
+        if (ascendingCounts[index][minNumber] != UNDEFINED_FLAG) {
+            return ascendingCounts[index][minNumber];
+        }
+
+        ascendingCounts[index][minNumber] = 0;
         for (int i = 0; i < minNumber + 1; i++) {
-            ascendingCounts[n][minNumber] += getAscendingNumCnt(n - 1, i);
+            ascendingCounts[index][minNumber] += getAscendingNumCnt(n - 1, i);
         }
 
-        ascendingCounts[n][minNumber] %= 10007;
+        ascendingCounts[index][minNumber] %= 10007;
 
-        return ascendingCounts[n][minNumber];
+        return ascendingCounts[index][minNumber];
     }
 }
 
